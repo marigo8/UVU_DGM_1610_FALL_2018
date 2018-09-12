2349e346b3b8543a409bfa61f804d1c639549cba
Mario Tijerino <mariogotijerino@gmail.com>
2018-09-12 17:16:10 -0600

    added jump functionality to CharacterMove.cs

--- DGM_1610_GAME/Assets/scripts/CharacterMove.cs
+++ DGM_1610_GAME/Assets/scripts/CharacterMove.cs
@@ -1,19 +1,46 @@
-﻿using System.Collections;
+﻿/*******************\
+	
+	CharacterMove
+
+\*******************/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
+	// Player Movement Variables
 	public int MoveSpeed;
 	public float JumpHeight;
+
+	// Player Grounded Variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
 
 	// Use this for initialization
 	void Start () {
 		print("Hello, World!");
 	}
+
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+	}
 	
 	// Update is called once per frame
 	void Update () {
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space) && Grounded){
+			Jump();
+		}
+
 		
+	}
+
+	// Character Jump Function
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
