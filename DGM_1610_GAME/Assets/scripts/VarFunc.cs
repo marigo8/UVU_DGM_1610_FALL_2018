f93842a9116bc85c3eb462b567c05303177336f4
Mario Tijerino <mariogotijerino@gmail.com>
2018-09-19 17:17:12 -0600

    Quiz 1 - VarFunc

---
+++ DGM_1610_GAME/Assets/scripts/VarFunc.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VarFunc : MonoBehaviour {
+
+	// 3 variables, vary access modifiers, 3 datatypes, assign at least 1 value
+
+	public float Health;
+	public float Shields;
+	public int Acceleration = 2;
+	private Transform PlayerTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// 3 functions
+
+	void PlayerTakeDamage(float Damage){
+		Health = Health - Damage*2/Shields;
+	}
+	void PlayerMoveRight(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x+Acceleration, GetComponent<Rigidbody2D>().velocity.y);
+	}
+	void PlayerMoveLeft(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x-Acceleration, GetComponent<Rigidbody2D>().velocity.y);
+	}
+}
