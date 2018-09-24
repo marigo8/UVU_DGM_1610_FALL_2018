c0caa53c7ed0801995efd9ed66122175e0ee39c7
Mario Tijerino <mariogotijerino@gmail.com>
2018-09-24 17:34:28 -0600

    Added Double Jump, Non-stick, and ScoreManager

--- DGM_1610_GAME/Assets/scripts/CharacterMove.cs
+++ DGM_1610_GAME/Assets/scripts/CharacterMove.cs
@@ -12,13 +12,19 @@ public class CharacterMove : MonoBehaviour {
 
 	// Player Movement Variables
 	public float MoveSpeed;
+	public float Acceleration;
+	public float MaxVelocity;
 	public float JumpHeight;
+	private bool DoubleJump;
 
 	// Player Grounded Variables
 	public Transform GroundCheck;
 	public float GroundCheckRadius;
 	public LayerMask WhatIsGround;
 	private bool Grounded;
+
+	// Non-Stick Player
+	private float MoveVelocity;
 
 	// Use this for initialization
 	void Start () {
@@ -39,16 +45,52 @@ public class CharacterMove : MonoBehaviour {
 				Jump();
 			}
 
+			// Double Jump
+			if(Grounded){
+				DoubleJump = false;
+			}
+			if( (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.W)) && !DoubleJump && !Grounded){
+				Jump();
+				DoubleJump = true;
+			}
+
+			// Non-Stick Player
+			if(Grounded){
+				MoveVelocity = 0f;
+			}
 			// Move Right
 			if(Input.GetKey (KeyCode.D)){
-				// velocity.x = velocity.x + MoveSpeed;
-				GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x+MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+				// velocity.x = velocity.x + Acceleration;
+				// GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x+Acceleration, GetComponent<Rigidbody2D>().velocity.y);
+				if(Grounded){
+					MoveVelocity = MoveSpeed;
+				}else{
+					MoveVelocity += MoveSpeed*0.1f;
+					if(MoveVelocity > MoveSpeed){
+						MoveVelocity = MoveSpeed;
+					}
+				}
 			}
 			// Move Left
 			if(Input.GetKey (KeyCode.A)){
-				// velocity.x = velocity.x - MoveSpeed;
-				GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+				// velocity.x = velocity.x - Acceleration;
+				// GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x-Acceleration, GetComponent<Rigidbody2D>().velocity.y);
+			    if(Grounded){
+					MoveVelocity = -MoveSpeed;
+				}else{
+					MoveVelocity -= MoveSpeed*0.1f;
+					if(MoveVelocity < -MoveSpeed){
+						MoveVelocity = -MoveSpeed;
+					}
+				}
 			}
+<<<<<<< Updated upstream
+=======
+
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+		
+>>>>>>> Stashed changes
 	}
 
 	// Character Jump Function
