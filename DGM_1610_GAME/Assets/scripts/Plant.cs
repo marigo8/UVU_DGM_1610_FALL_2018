2bb79d31e79c15fdc5054162fa651449d174103b
Mario Tijerino <mariogotijerino@gmail.com>
2018-10-12 15:02:20 -0600

    Created Plant inheritance

---
+++ DGM_1610_GAME/Assets/scripts/Plant.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour {
+
+	public float Age;
+    public float Size;
+    public string SeedType;
+	
+}
---
+++ DGM_1610_GAME/Assets/scripts/Tree.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : Plant {
+
+	public int branches;
+    public string BarkStyle;
+    public color BarkColor;
+    public color LeafColor;
+	
+}
