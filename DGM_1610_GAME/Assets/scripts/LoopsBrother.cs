17855997b27b5debcc003095b906e4992b6d7903
Mario Tijerino <mariogotijerino@gmail.com>
2018-10-26 14:43:24 -0600

    Did loops homework

--- DGM_1610_GAME/Assets/scripts/LoopsBrother.cs
+++ DGM_1610_GAME/Assets/scripts/LoopsBrother.cs
@@ -7,6 +7,16 @@ public class LoopsBrother : MonoBehaviour {
 	public int NumberOfAnts = 40;
 
 	public string Color = "Blue";
+	
+	private bool IsBlack;
+
+	public int CrackerCount = 1000;
+
+	private bool IsTikTokStupid = true;
+
+	private string WilhelmScream = "";
+
+	public int TimeLeftToDoProject;
 
 	// Use this for initialization
 	void Start () {
@@ -20,11 +30,51 @@ public class LoopsBrother : MonoBehaviour {
 			print(NumberOfAnts + " Ants marching");
 			NumberOfAnts--;
 		}
-
+		
 		// while(Color == "Red"){
 		// 	print("Color is " + Color);
 			
 		// }
+
+		/**************\
+		|	HOMEWORK   |
+		\**************/
+
+		// Checkerboard Generator (for loops 1 and 2)
+		for(int x = 0; x < 8; x++){ // x axis
+			for(int y = 0; y < 8; y++){ // y axis
+				IsBlack = !IsBlack;
+				if(IsBlack){
+					print("Square (" + x + "," + y + ") is Black");
+				}else{
+					print("Square (" + x + "," + y + ") is White");
+				}
+			}
+		}
+		// for loop 3
+		for(int i = 0; i < 100; i++){
+			WilhelmScream += "A";
+		}
+		print(WilhelmScream);
+		// while loop 1
+		while(!IsTikTokStupid){
+			// This Code will never run because Tik Tok is stupid.
+			IsTikTokStupid = true; // just in case.
+			print("something went wrong.");
+		}
+
+		// while loop 2
+		while(CrackerCount > 0){
+			CrackerCount--;
+			print("I have eaten a cracker and have "+ CrackerCount +" left.");
+		}
+
+		// while loop 3
+		while(TimeLeftToDoProject > 0){
+			print("I'll work on it later.");
+			TimeLeftToDoProject--;
+		}
+
 	}
 	
 	// Update is called once per frame
