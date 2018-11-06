5315929245efa0b38e44b78a30b1110a35b4acb2
Mario Tijerino <mariogotijerino@gmail.com>
2018-11-05 17:11:42 -0700

    Learned Arrays, created CameraFollow.cs, updated todo.txt

---
+++ DGM_1610_GAME/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public CharacterMove PC;
+
+	public bool IsFollowing;
+
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		PC = FindObjectOfType<CharacterMove>();
+
+		IsFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.L)){
+			IsFollowing = !IsFollowing;
+		}
+
+		if(IsFollowing){
+			transform.position = new Vector3(PC.transform.position.x + xOffset, PC.transform.position.y + yOffset, transform.position.z);
+		}
+	}
+}
---
+++ DGM_1610_GAME/Assets/scripts/Homework/Arrays.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+	
+	public int[] Eggs = new int[12];
+
+	public string[] Jedi;
+
+	// Use this for initialization
+	void Start () {
+
+		Eggs[0] = 1;
+		Eggs[1] = 2;
+		Eggs[2] = 3;
+		Eggs[3] = 4;
+		Eggs[4] = 5;
+		Eggs[5] = 6;
+		Eggs[6] = 7;
+		Eggs[7] = 8;
+		Eggs[8] = 9;
+		Eggs[9] = 10;
+		Eggs[10] = 11;
+		Eggs[11] = 12;
+
+		print(Eggs[11]);
+
+		Jedi = new string[6];
+
+		Jedi[0] = "Obi Wan Kenobi";
+		Jedi[1] = "Mace Windu";
+		Jedi[2] = "Luke Skywalker";
+		Jedi[3] = "Yoda";
+		Jedi[4] = "Ahsoka Tano";
+		Jedi[5] = "Kit Fisto";
+
+		foreach(string item in Jedi){
+			print("Jedi Master " + item);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
