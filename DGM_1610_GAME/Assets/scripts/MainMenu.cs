328325821ee4fd7c66e776d7f4b612fa7f7265e0
Mario Tijerino <mariogotijerino@gmail.com>
2018-11-26 17:12:36 -0700

    Created Main Menu

---
+++ DGM_1610_GAME/Assets/scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public int LevelToLoad;
+
+	public void LoadLevel(){
+		SceneManager.LoadScene(LevelToLoad);
+	}
+	public void LevelExit(){
+		Application.Quit();
+	}
+}
