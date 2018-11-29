5314baa3fe10afe3fbe1090c59d12e5e700b9911
Mario Tijerino <mariogotijerino@gmail.com>
2018-11-29 16:17:03 -0700

    Created Win Screen (not yet functional)

---
+++ DGM_1610_GAME/Assets/scripts/WinScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayAgain(){
+		SceneManager.LoadScene(1);
+	}
+
+	public void GoToMainMenu(){
+		SceneManager.LoadScene(0);
+	}
+}
