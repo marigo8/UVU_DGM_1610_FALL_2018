c9bba9dbdc687c98ce29ee2fad7501da5b6241d5
Mario Tijerino <mariogotijerino@gmail.com>
2018-12-04 16:43:37 -0700

    Created OutOfBounds.cs

---
+++ DGM_1610_GAME/Assets/scripts/OutOfBounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour {
+
+	public LevelManager LevelManager;
+
+	// Use this for initialization
+	void Start () {
+		LevelManager = FindObjectOfType <LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D other) {
+		
+		if(!Pause.Paused){
+			if(other.name == "PC"){
+				LevelManager.RespawnPlayer();
+			}
+			else if(other.name != "GroundCheck"){
+				print("Destroying "+other.name);
+				Destroy(other.gameObject);
+			}
+		}
+	}
+}
