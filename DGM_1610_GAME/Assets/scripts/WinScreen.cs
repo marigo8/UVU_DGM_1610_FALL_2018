45ee1c696e61dd577911392a448b861fd3f32eaf
Mario Tijerino <mariogotijerino@gmail.com>
2018-12-06 15:20:16 -0700

    added total score to win screen

--- DGM_1610_GAME/Assets/scripts/WinScreen.cs
+++ DGM_1610_GAME/Assets/scripts/WinScreen.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WinScreen : MonoBehaviour {
 
+	public Text SubTotalScoreText;
+	public Text HealthText;
+	public Text AmmoText;
+	public Text ScoreText;
+
+	public float ScoreDelay;
+
+	public int TotalScore;
+
 	// Use this for initialization
 	void Start () {
+		Time.timeScale = 1;
+		TotalScore = ScoreManager.Score + AmmoManager.AmmoCount + HealthManager.Health;
+		StartCoroutine("DisplayScore");
+		
 		
 	}
 	
@@ -27,5 +41,19 @@ public class WinScreen : MonoBehaviour {
 
 	public void GoToMainMenu(){
 		SceneManager.LoadScene(0);
+	}
+
+	public IEnumerator DisplayScore(){
+		yield return new WaitForSeconds(ScoreDelay);
+		SubTotalScoreText.text = ""+ScoreManager.Score;
+
+		yield return new WaitForSeconds(ScoreDelay);
+		HealthText.text = "+"+HealthManager.Health;
+
+		yield return new WaitForSeconds(ScoreDelay);
+		AmmoText.text = "+"+AmmoManager.AmmoCount;
+
+		yield return new WaitForSeconds(ScoreDelay);
+		ScoreText.text = ""+TotalScore;
 	}
 }
