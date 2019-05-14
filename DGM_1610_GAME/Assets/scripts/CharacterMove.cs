226435c227637f96bced844d8c68d6932cba6e43
Mario Tijerino <mariogotijerino@gmail.com>
2019-05-14 15:22:59 -0600

    made many changes to CHaracterMove.cs

--- DGM_1610_GAME/Assets/scripts/CharacterMove.cs
+++ DGM_1610_GAME/Assets/scripts/CharacterMove.cs
@@ -10,10 +10,14 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
+	// Rigidbody2D
+	private Rigidbody2D rb;
+
 	// Player Movement Variables
 	public float MoveSpeed;
 	public float SprintModifier;
 	public float AerialMoveSpeedModifier;
+	public float FallModifier;
 	public float MaxVelocity;
 	public float JumpHeight;
 	private bool DoubleJump;
@@ -37,6 +41,7 @@ public class CharacterMove : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		rb = GetComponent<Rigidbody2D>();
 		PlayerScale = transform.localScale;
 	}
 
@@ -51,6 +56,12 @@ public class CharacterMove : MonoBehaviour {
 	void FixedUpdate () {
 		if(!Pause.Paused){
 			if(!LevelManager.PlayerIsDead){
+
+				// Non-Stick Player
+				if(Grounded){
+					MoveVelocity = 0f;
+					rb.velocity = new Vector3(0,rb.velocity.y);
+				}
 				// Sprint
 				if(Input.GetKey (KeyCode.LeftShift)){
 					MoveSpeedModifier = SprintModifier;
@@ -70,29 +81,24 @@ public class CharacterMove : MonoBehaviour {
 					Jump();
 					DoubleJump = true;
 				}
-
-				// Non-Stick Player
-				if(Grounded){
-					MoveVelocity = 0f;
-				}
 				// Move Right
 				if(Input.GetKey (KeyCode.D)){
 					// velocity.x = velocity.x + Acceleration;
-					// GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x+Acceleration, GetComponent<Rigidbody2D>().velocity.y);
+					// rb.velocity = new Vector2(rb.velocity.x+Acceleration, rb.velocity.y);
 					if(Grounded){ // if grounded...
-						MoveVelocity = MoveSpeed*MoveSpeedModifier; // move normally (fixed velocity)
+						MoveVelocity += MoveSpeed*MoveSpeedModifier; // move normally (fixed velocity)
 						AnimatorObj.SetBool("IsRunning", true);
 					}else{ // if in the air
-						GetComponent<Rigidbody2D>().AddForce(new Vector2(MoveSpeed*MoveSpeedModifier*Time.deltaTime * AerialMoveSpeedModifier,0));
-						if(GetComponent<Rigidbody2D>().velocity.x > MoveSpeed*MoveSpeedModifier){
-							GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed*MoveSpeedModifier, GetComponent<Rigidbody2D>().velocity.y);
+						rb.AddForce(new Vector2(MoveSpeed*MoveSpeedModifier*Time.deltaTime * AerialMoveSpeedModifier,0));
+						if(rb.velocity.x > MoveSpeed*MoveSpeedModifier){
+							rb.velocity = new Vector2(MoveSpeed*MoveSpeedModifier, rb.velocity.y);
 						}
-						// MoveVelocity = GetComponent<Rigidbody2D>().velocity.x + MoveSpeed*MoveSpeedModifier*0.1f; // move with dynamic velocity
+						// MoveVelocity = rb.velocity.x + MoveSpeed*MoveSpeedModifier*0.1f; // move with dynamic velocity
 						// if(MoveVelocity > MoveSpeed*MoveSpeedModifier){ // but cap it if it gets too high
 						// 	MoveVelocity = MoveSpeed*MoveSpeedModifier;
 						// }
-						// GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
-						// GetComponent<Rigidbody2D>().AddForce(new Vector2(MoveSpeed*MoveSpeedModifier*4,0));
+						// rb.velocity = new Vector2(MoveVelocity, rb.velocity.y);
+						// rb.AddForce(new Vector2(MoveSpeed*MoveSpeedModifier*4,0));
 					}
 					// If player is not facing right
 					if(!FacingRight){
@@ -109,22 +115,22 @@ public class CharacterMove : MonoBehaviour {
 				// Move Left
 				if(Input.GetKey (KeyCode.A)){
 					// velocity.x = velocity.x - Acceleration;
-					// GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x-Acceleration, GetComponent<Rigidbody2D>().velocity.y);
+					// rb.velocity = new Vector2(rb.velocity.x-Acceleration, rb.velocity.y);
 					if(Grounded){
-						MoveVelocity = -MoveSpeed*MoveSpeedModifier;
+						MoveVelocity += -MoveSpeed*MoveSpeedModifier;
 						AnimatorObj.SetBool("IsRunning", true);
 					}else{
-						GetComponent<Rigidbody2D>().AddForce(new Vector2(-MoveSpeed*MoveSpeedModifier*Time.deltaTime * AerialMoveSpeedModifier,0));
-						if(GetComponent<Rigidbody2D>().velocity.x < -MoveSpeed*MoveSpeedModifier){
-							GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed*MoveSpeedModifier, GetComponent<Rigidbody2D>().velocity.y);
+						rb.AddForce(new Vector2(-MoveSpeed*MoveSpeedModifier*Time.deltaTime * AerialMoveSpeedModifier,0));
+						if(rb.velocity.x < -MoveSpeed*MoveSpeedModifier){
+							rb.velocity = new Vector2(-MoveSpeed*MoveSpeedModifier, rb.velocity.y);
 						}
-						// MoveVelocity = GetComponent<Rigidbody2D>().velocity.x - MoveSpeed*MoveSpeedModifier*0.05f;
+						// MoveVelocity = rb.velocity.x - MoveSpeed*MoveSpeedModifier*0.05f;
 						// if(MoveVelocity < -MoveSpeed*MoveSpeedModifier){
 						// 	MoveVelocity = -MoveSpeed*MoveSpeedModifier;
 						// }
 
-						// GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
-						// GetComponent<Rigidbody2D>().AddForce(new Vector2(-MoveSpeed*MoveSpeedModifier*4,0));
+						// rb.velocity = new Vector2(MoveVelocity, rb.velocity.y);
+						// rb.AddForce(new Vector2(-MoveSpeed*MoveSpeedModifier*4,0));
 					}
 					// If player is facing right
 					if(FacingRight){
@@ -140,16 +146,19 @@ public class CharacterMove : MonoBehaviour {
 				}
 				if(Grounded){
 					AnimatorObj.SetBool("IsGrounded",true);
-					GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
-					MoveVelocity = 0;
-					// GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+					//rb.velocity = new Vector2(MoveVelocity, rb.velocity.y);
+					transform.position += new Vector3(MoveVelocity,0,0) * Time.deltaTime;
+					// rb.velocity = new Vector2(MoveVelocity, rb.velocity.y);
 				}
 				else{
 					AnimatorObj.SetBool("IsGrounded",false);
+					if(rb.velocity.y < 0){
+						rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * FallModifier);
+					}
 				}
 			}
 			else{
-				GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+				rb.velocity = new Vector2(0,0);
 			}
 		}
 	}
@@ -160,6 +169,18 @@ public class CharacterMove : MonoBehaviour {
 	// Character Jump Function
 	public void Jump(){
 		// velocity.y = JumpHeight;
-		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+		float xSpeed = 0;
+		if(Grounded){
+			if(Input.GetKey(KeyCode.D)){
+				xSpeed += MoveSpeed*MoveSpeedModifier;
+			}
+			if(Input.GetKey(KeyCode.A)){
+				xSpeed += -MoveSpeed*MoveSpeedModifier;
+			}
+		}
+		else{
+			xSpeed = rb.velocity.x;
+		}
+		rb.velocity = new Vector2(xSpeed, JumpHeight);
 	}
 }
